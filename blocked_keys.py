5e0daf387df9da23b18c38712806e097050b9535
Nabil Abdelouahed <114498779+NabilAbdelouahed@users.noreply.github.com>
2025-01-17 10:09:28 +0100

    add blocked keys file

---
+++ blocked_keys.py
@@ -0,0 +1,48 @@
+keys = [
+    # Alphanumeric Keys
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    
+    # Function Keys
+    "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12",
+    
+    # Modifier Keys
+    "shift",
+    "ctrl",
+    "alt", 
+    "altgr",
+    "cmd",
+    "Win", "windows gauche",
+    "right windows", "right shift", "alt gr", "left shift", "right ctrl", "left ctrl", "left alt", "left windows", "windows",
+    
+    # Navigation Keys
+    #"up", "down", "left", "right",
+    #"home", "end", "pageup", "pagedown",
+    
+    # Editing Keys
+    "delete", "insert",
+    "tab", "space", "enter",
+    
+    # Lock Keys
+    #"capslock",
+    
+    # Symbol and Miscellaneous Keys
+    "`", "-", "=", "[", "]", "\\", ";", "'", ",", ".", "/",  # Standard symbols
+    "grave", "equal", "backslash", "semicolon", "apostrophe", "comma", "dot", "slash",
+    
+    # Numeric Keypad Keys
+    #"num_0", "num_1", "num_2", "num_3", "num_4", "num_5", "num_6", "num_7", "num_8", "num_9",
+    "num_divide", "num_multiply", "num_add", "num_enter",
+    
+    # Application and System Keys
+    "pause", "menu", "application",
+    
+    # Special Keys
+    "esc", "escape"
+]
+
+
+
+
+
+
+
